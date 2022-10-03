e23248ca78820ed256bac6f190fa8714ec8a3cd1
gondhijagapathi <gondhijagapathi@gmail.com>
2022-10-03 12:34:08 +0530

    Basic UI elements done

---
+++ src/main/AddItem.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import ShareIcon from '@mui/icons-material/Share';
+import { Paper } from '@mui/material';
+
+const actions = [
+  { icon: <FileCopyIcon />, name: 'Copy' },
+  { icon: <SaveIcon />, name: 'Save' },
+  { icon: <PrintIcon />, name: 'Print' },
+  { icon: <ShareIcon />, name: 'Share' },
+];
+
+export default function AddItem() {
+  return (
+    <Paper sx={{ position: 'fixed', bottom: 50, left: 0, right: 0 }} elevation={3}>
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+          />
+        ))}
+      </SpeedDial>
+    </Paper>
+  );
+}
--- src/main/DashBoard.js
+++ src/main/DashBoard.js
@@ -1,10 +1,23 @@
-import React from "react";
+import * as React from 'react';
+import BottomNav from './BottomNav';
+import AddItem from './AddItem';
+import ItemsList from './ItemsList';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 function DashBoard() {
-  return (
-    <div>
-    </div>
-  );
+    const darkTheme = createTheme({
+        palette: {
+            mode: 'dark',
+        },
+    });
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <ItemsList />
+            <AddItem />
+            <BottomNav />
+        </ThemeProvider>
+    );
 }
 
 export default DashBoard;
